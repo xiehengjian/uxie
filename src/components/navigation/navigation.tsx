c7a87507b0ddbb1a4ddb53dd238945aafa6c71f5
xiehengjian <846188037@qq.com>
2024-08-17 10:53:14 +0000

    s

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -2,7 +2,7 @@ import { ElementRef, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
-import { SidebarIcon } from '@blocksuite/icons/dist/rc';
+import { SidebarIcon } from '@blocksuite/icons/rc';
 
 export const Navigation = () => {
     const pathname = usePathname()
