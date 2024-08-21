493bc7d56463ca175ab1ffb8d4be4339ef460eea
xiehengjian <846188037@qq.com>
2024-08-21 05:03:10 +0000

    s

--- src/pages/f/[folderId]/[docId].tsx
+++ src/pages/f/[folderId]/[docId].tsx
@@ -15,9 +15,18 @@ const DynamicDocViewerPage = () => {
 
 const WorkspaceComponent = dynamic(
   (() => {
-    if (typeof window !== "undefined") {
-      return import("@/components/workspace");
-    }
+      if (typeof window !== "undefined") {
+          console.log('window is defined, start importing Workspace component');
+          return import("@/components/workspace").then((module) => {
+              console.log('Successfully imported Workspace component');
+              return module;
+          }).catch((error) => {
+              console.error('Error when importing Workspace component', error);
+              throw error;
+          });
+      }
+      console.log('window is not defined. Failed to import Workspace component');
+      return () => null; // Return a null component if window is undefined
   }) as any,
   { ssr: false },
 );
