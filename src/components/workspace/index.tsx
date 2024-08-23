75d3b037b9ca8deaa4c2b036d3ef68f27eb6a446
xiehengjian <846188037@qq.com>
2024-08-23 18:22:56 +0000

    s

--- src/components/workspace/index.tsx
+++ src/components/workspace/index.tsx
@@ -46,9 +46,6 @@ const DocViewerPage = () => {
   return (
     <>
       <ResizablePanelGroup autoSaveId="window-layout" direction="horizontal">
-
-        <Navigation />
-
         <ResizablePanel defaultSize={50} minSize={30}>
           <div className="h-screen min-w-[25vw] border-stone-200 bg-white sm:rounded-lg sm:border-r sm:shadow-lg">
             <DocViewer doc={doc} canEdit={doc.userPermissions.canEdit} />
