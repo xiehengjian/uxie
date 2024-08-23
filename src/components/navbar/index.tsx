a0b4ecf427b1c7074cbe6624c498000231821635
xiehengjian <846188037@qq.com>
2024-08-23 18:24:37 +0000

    s

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,12 +10,6 @@ const Navbar = () => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col px-4 py-4 lg:px-16 2xl:max-w-7xl">
       <div className="flex items-center justify-between bg-opacity-30 backdrop-blur-lg backdrop-filter">
-        <Link href="/">
-          <div className="flex items-center gap-2">
-            <Image alt="Uxie" src="/logo.png" width={40} height={40} />
-            {/* <span className="font-semibold">Uxie</span> */}
-          </div>
-        </Link>
         {session ? (
           <UserAccountNav user={session.user} />
         ) : (
